661169606cbcea1316e151a63e49533fb7fd39e4
Tom Willkens <tom.willkens@gmail.com>
2018-03-18 18:14:50 -0400

    fix

--- user_server.rb
+++ user_server.rb
@@ -12,11 +12,11 @@ get '/hello' do
   return_message.to_json
 end
 
-#s = Rufus::Scheduler.singleton
-#
-#s.every '3s' do
-#  puts 'hi'
-#  client = UserClient.new 'tom'
-#  client.get_hello
-#end
+s = Rufus::Scheduler.singleton
+
+s.every '3s' do
+  puts 'hi'
+  client = UserClient.new 'tom'
+  client.get_hello
+end
 
