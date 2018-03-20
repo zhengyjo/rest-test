ee77c851934afa8685819881dec575c49eb679f9
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-03-20 17:29:24 -0400

    constrain length

--- user_client.rb
+++ user_client.rb
@@ -6,34 +6,19 @@ class UserClient
 
   def initialize name
     @name = name
-    @conversation = {}
-    @counter = 0
+    @log = []
   end
-
-#  def join_game
-#    response = RestClient.post 'http://localhost:8080/join', :data => {name: @name}.to_json, :accept => :json
-#    puts JSON.parse(response,:symbolize_names => true)
-#  end
-#
-#  def get_cards
-#    response = RestClient.get 'http://localhost:8080/cards', {:params => {:name => @name}}
-#    puts response
-#  end
-#
-#  def deal
-#    response = RestClient.post 'http://localhost:8080/deal', :data =>{}.to_json, :accept => :json
-#    puts response
-#  end
 
   def get_hello
-    response = RestClient.get 'http://protected-bastion-93597.herokuapp.com/hello', {}
+    response = RestClient.get 'https://cryptic-anchorage-60369.herokuapp.com/hello', {}
     resp_hash = JSON.parse(response)
-    @counter = @counter + 1
-    key = @counter.to_s
-    @conversation[key] = resp_hash['hello']
-    puts @conversation.inspect
-    puts response
+    key = Time.now.to_s
+    entry = {}
+    entry[key] = resp_hash['hello']
+    @log.push(entry)
+    if @log.length > 50
+      @log.shift
+    end
+    puts @log.inspect
   end
-
-
 end
